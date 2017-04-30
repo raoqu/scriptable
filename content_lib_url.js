fc9d5245c4c0d833f99ec1c2544e2222bd73a6cb
饶趣 <qu.raoq@alibaba-inc.com>
2017-04-30 23:14:10 +0800

    解决下载文件路径为空的问题

--- content_lib_url.js
+++ content_lib_url.js
@@ -13,30 +13,30 @@ class UrlUtils {
 	//  base: page url base of the relative url
 	static resolveUrl(url, base){
 		if('string'!==typeof url || !url){
-				return null; // wrong or empty url
+			return null; // wrong or empty url
 		}
 		else if(url.match(/^[a-z]+\:\/\//i)){ 
-				return url; // url is absolute already 
+			return url; // url is absolute already 
 		}
 		else if(url.match(/^\/\//)){ 
-				return 'http:'+url; // url is absolute already 
+			return 'http:'+url; // url is absolute already 
 		}
 		else if(url.match(/^[a-z]+\:/i)){ 
-				return url; // data URI, mailto:, tel:, etc.
+			return url; // data URI, mailto:, tel:, etc.
 		}
 		else if('string'!==typeof base){
-				var a=document.createElement('a'); 
-				a.href=url; // try to resolve url without base  
-				if(!a.hostname || !a.protocol || !a.pathname){ 
-						return null; // url not valid 
-				}
-				return 'http://'+url;
+			var a=document.createElement('a'); 
+			a.href=url; // try to resolve url without base  
+			if(!a.hostname || !a.protocol || !a.pathname){ 
+					return null; // url not valid 
+			}
+			return 'http://'+url;
 		}
 		else{ 
-				base = UrlUtils.resolveUrl(base); // check base
-				if(base===null){
-						return null; // wrong base
-				}
+			base = UrlUtils.resolveUrl(base); // check base
+			if(base===null){
+				return null; // wrong base
+			}
 		}
 
 		return UrlUtils.compose(base, url);
@@ -48,25 +48,25 @@ class UrlUtils {
 		a.href=base;
 
 		if(url[0]==='/'){ 
-				base=[]; // rooted path
+			base=[]; // rooted path
 		}
 		else{ 
-				base=a.pathname.split('/'); // relative path
-				base.pop(); 
+			base=a.pathname.split('/'); // relative path
+			base.pop(); 
 		}
 		url=url.split('/');
 		for(var i=0; i<url.length; ++i){
-				if(url[i]==='.'){ // current directory
-						continue;
+			if(url[i]==='.'){ // current directory
+					continue;
+			}
+			if(url[i]==='..'){ // parent directory
+				if('undefined'===typeof base.pop() || base.length===0){ 
+						return null; // wrong url accessing non-existing parent directories
 				}
-				if(url[i]==='..'){ // parent directory
-						if('undefined'===typeof base.pop() || base.length===0){ 
-								return null; // wrong url accessing non-existing parent directories
-						}
-				}
-				else{ // child directory
-						base.push(url[i]); 
-				}
+			}
+			else{ // child directory
+				base.push(url[i]); 
+			}
 		}
 		return a.protocol+'//'+a.hostname+base.join('/');
 	}
@@ -77,11 +77,11 @@ class UrlUtils {
 		var path = a.pathname;
 		if( path ) {
 			var stack = path.split('/');
-			stack.length && !stack[0] && stack.shift(); // remove first empty string
-			folder && stack.unshift(folder);
 			if( stack ) {
-				if( stack.length>0 && !stack[0])
-					stack.shift();
+				stack.length && !stack[0] && stack.shift(); // remove first empty string
+				! stack.length && stack.push(BaseUtils.uniqId());
+				folder && stack.unshift(folder);
+				stack.length && !stack[stack.length-1] && (stack[stack.length-1] = BaseUtils.uniqId()); // filename should not be empty
 				path = stack.join('/');
 			}
 		}
